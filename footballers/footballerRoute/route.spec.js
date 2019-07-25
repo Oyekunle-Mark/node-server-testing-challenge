676707502458fd02e6449b79678c2da63d9249f0
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-25 15:54:21 +0100

    Test for the create footballer endpoint

---
+++ footballers/footballerRoute/route.spec.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const server = require('../../api/server');
+
+describe('POST /footballers', () => {
+  it('Returns a 201', () =>
+    request(server)
+      .post('/api/footballers')
+      .send({
+        name: 'Andrea Pirlo',
+        position: 'Midfielder',
+      })
+      .expect('Content-Type', /json/)
+      .expect(201));
+
+  it('Successfully creates a user', () =>
+    request(server)
+      .post('/api/footballers')
+      .send({
+        name: 'Andrea Pirlo',
+        position: 'Midfielder',
+      })
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then(res => {
+        expect(res.body).toEqual({
+          id: 1,
+          name: 'Andrea Pirlo',
+          position: 'Midfielder',
+        });
+      }));
+});
