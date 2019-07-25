f9b5a79d418ab0ce86966cb95abcf23724fe41a2
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-25 13:54:26 +0100

    Test to test bad url

--- api/server.spec.js
+++ api/server.spec.js
@@ -17,3 +17,20 @@ describe('/ Returns a message that server is up', () => {
         expect(res.body.message).toEqual('Welcome, Thou faithful developer.');
       }));
 });
+
+describe('/ Catch bad URLs and respond with appropriate message', () => {
+  it('returns a status 404', () =>
+    request(server)
+      .get('/awfulUrl')
+      .expect('Content-Type', /json/)
+      .expect(404));
+
+  it('responds with a message', () =>
+    request(server)
+      .get('/awfulUrl')
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .then(res => {
+        expect(res.body.message).toEqual('Bad bad URL mate!');
+      }));
+});
